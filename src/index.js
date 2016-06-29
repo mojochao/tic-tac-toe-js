5afad8d0f7b23947c399103e9d007014b2cfbf0f
Allen Gooch <agooch@samba.tv>
2016-06-29 15:21:45 -0500

    Remove unnecessary output.

--- src/index.js
+++ src/index.js
@@ -11,16 +11,15 @@ const PLAYER2 = 'o'
 const game = new Game(PLAYER1, PLAYER2, DIM)
 game.onPlayer1Turn = (available) => {
     const play = available[randomInt(0, available.length - 1)]
-    console.log(`player '${PLAYER1}' playing ${play}`)
     game.playTurn(play, PLAYER1)
 }
 game.onPlayer2Turn = (available) => {
   const play = available[randomInt(0, available.length - 1)]
-  console.log(`player '${PLAYER2}' playing ${play}`)
   game.playTurn(play, PLAYER2)
 }
 game.onGameOver = (winner) => {
-  console.log(`game over and winner is '${winner}'\n`)
+  const player = winner === null ? 'no one' : `'${winner}'`
+  console.log(`game over and winner is ${player}\n`)
 }
 
 console.log('game starting\n')
