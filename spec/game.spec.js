1a45bdfdeda61656dc8df618d82bdb8b2e3196e9
Allen Gooch <agooch@samba.tv>
2016-06-30 10:31:47 -0500

    Improve readability of Game test.

--- spec/game.spec.js
+++ spec/game.spec.js
@@ -2,23 +2,10 @@ import test from 'tape'
 
 import Game from '../src/Game'
 
-function* turnsWherePlayerOneWins() {
-  const turns = [0, 3, 1, 4, 2] // player one wins on top horizontal line
-  for (let turn of turns) yield turn
-}
-
-function* turnsWherePlayerTwoWins() {
-  const turns = [3, 0, 4, 1, 6, 2] // player two wins on top horizontal line
-  for (let turn of turns) yield turn
-}
-
-function* turnsWhereNoPlayerWins() {
-  const turns = [4, 0, 3, 5, 2, 6, 7, 1, 8] // no player wins
-  for (let turn of turns) yield turn
-}
+const iterator = arr => arr[Symbol.iterator]()
 
 test('Game where player one wins', (t) => {
-  const turns = turnsWherePlayerOneWins()
+  const turns = iterator([0, 3, 1, 4, 2])
   const game = new Game('x', 'o', 3)
   game.onPlayerOneTurn = available => game.takePlayerOneTurn(turns.next().value)
   game.onPlayerTwoTurn = available => game.takePlayerTwoTurn(turns.next().value)
@@ -31,7 +18,7 @@ test('Game where player one wins', (t) => {
 })
 
 test('Game where player two wins', (t) => {
-  const turns = turnsWherePlayerTwoWins()
+  const turns = iterator([3, 0, 4, 1, 6, 2])
   const game = new Game('x', 'o', 3)
   game.onPlayerOneTurn = available => game.takePlayerOneTurn(turns.next().value)
   game.onPlayerTwoTurn = available => game.takePlayerTwoTurn(turns.next().value)
@@ -44,7 +31,7 @@ test('Game where player two wins', (t) => {
 })
 
 test('Game where no player wins', (t) => {
-  const turns = turnsWhereNoPlayerWins()
+  const turns = iterator([4, 0, 3, 5, 2, 6, 7, 1, 8])
   const game = new Game('x', 'o', 3)
   game.onPlayerOneTurn = available => game.takePlayerOneTurn(turns.next().value)
   game.onPlayerTwoTurn = available => game.takePlayerTwoTurn(turns.next().value)
