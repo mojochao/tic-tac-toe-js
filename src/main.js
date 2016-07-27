7bff4afe1ae03bb9686676742d3e3ddc5aed51a3
Allen Gooch <agooch@samba.tv>
2016-07-27 10:38:19 -0500

    Add comments to util function.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Game from './Game'
 
+// Returns random integer between min and max (exclusive)
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min
-const randomItem = (arr) => arr[randomInt(0, arr.length - 1)]
+
+const randomItem = (arr) => arr[randomInt(0, arr.length)]
 
 const game = new Game('x', 'o', 3)
 game.onPlayerOneTurn = (available) => game.takePlayerOneTurn(randomItem(available))
